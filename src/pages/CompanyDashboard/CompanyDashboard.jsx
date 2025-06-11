1042fbab9a7514f485b99dd538996f04249ace6c
Najeeb565 <najeebbhatti565@gmail.com>
2025-06-11 22:39:59 +0500

    Request sent page done

---
+++ src/pages/CompanyDashboard/CompanyDashboard.jsx
@@ -0,0 +1,12 @@
+import Setadmin from "./setadmin/setadmin.jsx"
+
+function dashboard (){
+    return (
+        <div>
+            <h1>Company Dashboard</h1>
+            <Setadmin />
+        </div>
+    )
+}
+
+export default dashboard
