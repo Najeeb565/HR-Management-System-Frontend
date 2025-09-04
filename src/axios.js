1bea31973cce49663e84e8e4c83e4b78c05e2d58
Najeeb565 <najeebbhatti565@gmail.com>
2025-09-04 23:22:57 +0500

    Added vercel.json for React Router rewrite and updated API env

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
 
 // Auto attach token to every request
 instance.interceptors.request.use((config) => {
